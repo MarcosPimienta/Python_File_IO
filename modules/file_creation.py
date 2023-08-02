b48210cefba685f433e1a5e18d5f7da4052ebb56
MarcosPimienta <fenix3819@gmail.com>
2023-08-02 16:07:31 -0500

    encapsulated into a function

--- modules/file_creation.py
+++ modules/file_creation.py
@@ -3,24 +3,26 @@ import random
 from pathlib import Path
 
 
-log_levels = ['INFO', 'WARNING', 'DEBUG', 'ERROR', 'CRITICAL']
-log_messages = ['System started', 'Process completed successfully',
-                'An error occurred']
+def generate_logs():
 
-number_logs = 1000
-path = Path('logs')
+    log_levels = ['INFO', 'WARNING', 'DEBUG', 'ERROR', 'CRITICAL']
+    log_messages = ['System started', 'Process completed successfully',
+                    'An error occurred']
 
-if not path.exists():
-    print(f'Creating directory {str(path)}...')
-    path.mkdir(parents=True, exist_ok=True)
+    number_logs = 1000
+    path = Path('logs')
 
-log_file = path / 'mylogfile.log'
+    if not path.exists():
+        print(f'Creating directory {str(path)}...')
+        path.mkdir(parents=True, exist_ok=True)
 
-with log_file.open('a', encoding="utf-8") as file:
-    for _ in range(number_logs):
-        level = random.choice(log_levels)
-        ts = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        msg = random.choice(log_messages)
+    log_file = path / 'mylogfile.log'
 
-        line_message = ts + " - " + level + " - " + msg + '\n'
-        file.write(line_message)
+    with log_file.open('a', encoding="utf-8") as file:
+        for _ in range(number_logs):
+            level = random.choice(log_levels)
+            ts = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            msg = random.choice(log_messages)
+
+            line_message = ts + " - " + level + " - " + msg + '\n'
+            file.write(line_message)
