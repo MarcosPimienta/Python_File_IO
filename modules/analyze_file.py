848ca5688d748a6b885ea53d31164db1c6937cab
MarcosPimienta <fenix3819@gmail.com>
2023-08-09 12:38:48 -0500

    modified with functional programming approach

--- modules/analyze_file.py
+++ modules/analyze_file.py
@@ -2,23 +2,28 @@
 from pathlib import Path
 
 
+def get_log_level(line):
+    return line.split(" - ")[1]
+
+
+def is_error_line(line):
+    return "error" in get_log_level(line).lower()
+
+
 def analyze_logs(file_path: Path):
     try:
         with open(file_path, 'r') as file:
             log_dict = {}
-            error_logs = []
+            lines = file.readlines()
 
-            for line in file.readlines():
-                parts = line.split(" - ")
-                log_level = parts[1]
+            log_levels = list(map(get_log_level, lines))
+            error_logs = list(filter(is_error_line, lines))
 
+            for log_level in log_levels:
                 if log_level in log_dict:
                     log_dict[log_level] += 1
                 else:
                     log_dict[log_level] = 1
-
-                if "error" in log_level.lower():
-                    error_logs.append(line)
 
         # Print out the results
         print("Log levels count:")
